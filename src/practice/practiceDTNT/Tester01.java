976e8e8f93ed4a2641acd042584357e0afa64bb8
unknown <BaharSonmez`>
2023-01-09 19:13:39 +0100

    V1

--- src/practice/practiceDTNT/Tester01.java
+++ src/practice/practiceDTNT/Tester01.java
@@ -3,6 +3,7 @@ package practice.practiceDTNT;
 public class Tester01 {
     public static void main(String[] args) {
         //Ex HH:40 tan 23 e kadar tum cift sayilari ekrana yazdiran kodu yaziniz.
+        int a = 40;
 
 
 
