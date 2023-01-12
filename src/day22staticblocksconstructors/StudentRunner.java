fa67d3d3488a75eb1c98a116c6ac6ffbaf26d281
unknown <BaharSonmez`>
2023-01-12 14:26:47 +0100

    b9

--- src/day22staticblocksconstructors/StudentRunner.java
+++ src/day22staticblocksconstructors/StudentRunner.java
@@ -5,7 +5,7 @@ public class StudentRunner {
 
 
         Student std1 = new Student("Ali Can",13,7,"'AC13G7","Istanbul");
-        Student std2 = new Student("Tom Hanks" 21 "TH21G12");
+        Student std2 = new Student("Tom Hanks" ,21 ,"TH21G12");
         System.out.println(std1);
         System.out.println(std2);
 
